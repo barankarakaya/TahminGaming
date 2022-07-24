5ad85c06d7fb0ae9a69128053151b0d18658ffc0
baran <bkarakaya0707@gmail.com>
2022-07-24 18:07:35 +0300

    Add project files.

---
+++ VeriYapilari/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeriYapilari
+{
+    class Program
+    {
+        static Random rastgele = new Random();
+        static void Main(string[] args)
+        {
+            int tahmin, sayi = rastgele.Next(100);
+
+            while (true)
+            {
+                Console.WriteLine("Bir Sayi Giriniz.");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+                if (tahmin>sayi)
+                {
+                    Console.WriteLine("Daha Küçük Bir Sayi Giriniz");
+                }
+                else if (tahmin<sayi)
+                {
+                    Console.WriteLine("Daha Büyük Bir Sayi Giriniz.");
+                }
+                else
+                {
+                    Console.WriteLine("Tebrikler Bildiniz.");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
